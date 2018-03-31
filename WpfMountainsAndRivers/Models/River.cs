d071c8c366c0e367c70ca048ad4606be698038b9
Chip Nevins <chip.nevins@gmail.com>
2018-03-31 10:05:17 -0400

    Some initial work

---
+++ WpfMountainsAndRivers/Models/River.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMountainsAndRivers.Models
+{
+    public class River: ObservableObject
+    {
+        #region Fields
+
+        string _riverId;
+        string _riverName;
+        int _milesLong;
+
+        #endregion
+
+        #region Property getters and setters
+        public string RiverId
+        {
+            get { return _riverId; }
+            set
+            {
+                if (value != _riverId)
+                {
+                    _riverId = value;
+                    OnPropertyChanged("RiverId");
+                }
+            }
+        }
+
+        public string RiverName {
+            get { return _riverName; }
+            set
+            {
+                if (value != _riverName)
+                {
+                    _riverName = value;
+                    OnPropertyChanged("RiverName");
+                }
+            }
+
+        }
+
+        public int MilesLong
+        {
+            get { return _milesLong; }
+            set
+            {
+                if (value != _milesLong)
+                {
+                    _milesLong = value;
+                    OnPropertyChanged("MilesLong");
+                }
+            }
+        }
+        #endregion Property getters and setters
+
+        public River (string riverId, string riverName, int milesLong )
+        {
+            this._riverId = riverId;
+            this._riverName = riverName;
+            this._milesLong = milesLong;
+        }
+
+    }
+}
---
+++ WpfMountainsAndRivers/MountainsAndRiversApp.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfMountainsAndRivers.Models;
+
+namespace WpfMountainsAndRivers
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class MountainsAndRiversApp : Application
+    {
+        private ObservableCollection<River> rivers = new ObservableCollection<River>();
+
+        void AppStartup(object sender, StartupEventArgs args)
+        {
+            LoadData();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        public ObservableCollection<River> Rivers
+        {
+            get { return this.rivers; }
+            set { this.rivers = value; }
+        }
+
+        private void LoadData()
+        {
+            for (int i = 1; i < 126; i++) {
+                string s = i.ToString();
+                Rivers.Add(new River("R" + s, "River " + s, i));
+            }
+        }
+
+    }
+}
